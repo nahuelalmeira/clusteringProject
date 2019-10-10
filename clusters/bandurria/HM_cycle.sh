08412ec141960782f18e955a5e7f99e12af73a8e
nahuelalmeira <nahu1990@gmail.com>
2019-10-10 08:55:34 -0300

    Corrected path

--- clusters/bandurria/HM_cycle.sh
+++ clusters/bandurria/HM_cycle.sh
@@ -4,13 +4,13 @@ N=$1
 alpha=$2
 MODE="maxC"
 randMCS=100
-inputDir="../../../networks/model/HM/HM_m4/HM_m4_N${N}/HM_m4_N${N}_alpha${alpha}"
+inputDir="../../networks/model/HM/HM_m4/HM_m4_N${N}/HM_m4_N${N}_alpha${alpha}"
 NETWORK="HM_m4_N${N}_alpha${alpha}_00000_gcc"
 nSamples=100
 TRANSIT=$3
 DECORR=$4
 SEED=0
 
-../annealing ${NETWORK} --inputDir ${inputDir} --mode ${MODE} --randMCS ${randMCS} \
-                        --transitTime ${TRANSIT} --decorrTime ${DECORR} \
-                        --samples ${nSamples}  --cycle --seed ${SEED}
+../../code/Cpp/annealing ${NETWORK} --inputDir ${inputDir} --mode ${MODE} --randMCS ${randMCS} \
+                                    --transitTime ${TRANSIT} --decorrTime ${DECORR} \
+                                    --samples ${nSamples}  --cycle --seed ${SEED}
